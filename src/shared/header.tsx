a6576547c5ccac4dd405bf23f77aade2fef2cc76
unknown <msegantine8@gmail.com>
2022-10-29 17:43:52 -0300

    Add Header component

--- src/shared/header.tsx
+++ src/shared/header.tsx
@@ -1,5 +1,44 @@
 import "../css/header.css";
 
 export const Header = () => {
-  return <h1>Header</h1>;
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="">Link</a>
+          </li>
+          <li>
+            <a href="">Link</a>
+          </li>
+          <li>
+            <a href="">Link</a>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="title-home">
+        <h1>Website Template</h1>
+        <span>Lading Page Design Template</span>
+      </div>
+
+      <div className="carrosel">
+        <nav>
+          <ul>
+            <span></span>
+            <span className="selected"></span>
+            <span></span>
+            <span></span>
+          </ul>
+        </nav>
+
+        <div className="content">
+          <h4>Lorem</h4>
+
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+        </div>
+      </div>
+    </header>
+  );
 };
