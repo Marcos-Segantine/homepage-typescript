e126d34f98fdbb346452ad1fbbda16171b4098ca
unknown <msegantine8@gmail.com>
2022-10-30 17:27:11 -0300

    Styling Footer

--- src/shared/footer.tsx
+++ src/shared/footer.tsx
@@ -1,5 +1,19 @@
 import "../css/footer.css";
 
 export const Footer = () => {
-  return <h1>footer</h1>;
+  return (
+    <footer>
+      <div className="logo">
+        <h1>Logo</h1>
+      </div>
+
+      <nav>
+        <ul>
+          <li><a href="">Link</a></li>
+          <li><a href="">Link</a></li>
+          <li><a href="">Link</a></li>
+        </ul>
+      </nav>
+    </footer>
+  )
 };
